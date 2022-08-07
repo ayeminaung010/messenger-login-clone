6b2012fd0de344694cb3c84f46251fdb52837d6f
ayeminaung010 <ayeminaung.mf@gmail.com>
2022-08-07 19:47:41 +0630

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+      // Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
+
+let button = document.getElementById("button");
+let closeSection = document.getElementById("closeSection");
+
+
+button.addEventListener("click", function(){
+    closeSection.style.display ="none";
+})
+    
